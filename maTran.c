8e1249740dd6c1e2d934605e81daffc202c587dc
tristannguye03 <57775796+tristannguye03@users.noreply.github.com>
2020-05-07 01:12:29 -0700

    Add files via upload

---
+++ maTran.c
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a[3] = { 2, 4, 1 };
+    int b[3] = {1, 2, 3 };
+    int i ;
+    int j=3;
+        cout<<"The matrix is:"<<endl;
+        for(i=0; i<= j-1; ++i)
+        {
+            cout << a[0] << " " << b[0] <<" ";
+            cout<<endl;
+        }
+        cout<<endl;
+        
+    
+    return 0;
+}
